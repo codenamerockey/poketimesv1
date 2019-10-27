cd0898c67f352fa9a85a7c94938299e47b013393
Rockey Simmons <codenamerockey@gmail.com>
2019-10-26 18:00:27 -0700

    added navbar, and 3 components home, about, and contact

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">This is the Contact Page</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis illum
+        maxime magnam? Rem ratione nesciunt temporibus fugiat aliquam. Ipsum
+        delectus quisquam nobis in maiores debitis rem suscipit, voluptates
+        error aliquam.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">This is the Home Page</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis illum
+        maxime magnam? Rem ratione nesciunt temporibus fugiat aliquam. Ipsum
+        delectus quisquam nobis in maiores debitis rem suscipit, voluptates
+        error aliquam.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
